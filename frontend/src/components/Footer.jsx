547caa94603fbd2444eeee533c9d0c53fd9703a4
Mohammad Shakin <md.shakin18@gmail.com>
2024-09-07 23:18:29 +0600

    updates

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Bite!</div>
+          <div className="right">
+            <p>Dhaka, Bangladesh</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Saber Shakin</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Shakin.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import { data } from "../restApi.json";
 const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <section className="team" id="team">
+      <div className="container">
+        <div className="heading_section">
+          <h1 className="heading">OUR TEAM</h1>
+          <p>Meet the talented chefs behind every dish! With a passion for flavor and creativity, our team crafts every meal with care, bringing you the best of culinary expertise. Taste the difference in every bite!
+          </p>
+        </div>
+        <div className="team_container">
+          {data[0].team.map((element) => {
+            return (
+              <div className="card" key={element.id}>
+                <img src={element.image} alt={element.name} />
+                <h3>{element.name}</h3>
+                <p>{element.designation}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Team
+export default Team;
