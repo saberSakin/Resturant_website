73307d0dcd8bb43a496c8eb2fbd94d1f6d276b45
Mohammad Shakin <md.shakin18@gmail.com>
2024-09-07 22:51:09 +0600

    updates

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+            At Bite! we're all about great food and easy online ordering. Our chefs whip up fresh, tasty dishes that hit the spot, no matter what you’re craving. From comfort classics to healthy bites, we’ve got something for everyone.
+            Order online and let us bring delicious food right to your door. Fast, fresh, and full of flavor—that's how we do it!
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {data} from '../restApi.json'
+const Menu = () => {
+  return (
+    <>
+      <section className='menu' id='menu'>
+        <div className="container">
+            <div className="heading_section">
+                <h1 className="heading">POPULAR DISHES</h1>
+                <p>Discover a variety of delicious dishes, made fresh to order. Whether you're in the mood for something classic or want to try something new, we've got flavors to satisfy every craving. Dig in and explore!</p>
+            </div>
+            <div className="dishes_container">
+                {
+                    data[0].dishes.map(element => (
+                        <div className="card" key={element.id}>
+                                <img src={element.image} alt={element.title} />
+                                <h3>{element.title}</h3>
+                                <button>{element.category}</button>
+                        </div>
+                    ))
+                }   
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Menu
